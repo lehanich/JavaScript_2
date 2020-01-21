6f249dbbe6bcabdca9db07b33916f4d628f9c15f
lehanich <ayakimkin@gmail.com>
2020-01-22 00:10:43 +0300

    Урок 2 копирование класса в проект

--- main.js
+++ main.js
@@ -8,12 +8,10 @@ class GoodsItem {
     render() {
         return `
             <div class="goods-item" data-id="${this.id}">
-                <div class="pImage">
-                    <img src="${this.img}" alt="alt">
-                </div>
+                <img src="${this.img}" alt="alt">
                 <h3>${this.title}</h3>
-                <p class="price">${this.price}</p>
-                <button name="btn-buy"  class="js-add-to-cart btn-buy">Добавить</button>
+                <p>${this.price}</p>
+                <button class="js-add-to-cart">Добавить</button>
             </div>
         `;
     }
@@ -63,30 +61,3 @@ class GoodsList {
 const list = new GoodsList('.goods-list');
 list.fetchGoods();
 list.render();
-
-
-
-// const goods = [
-//     { title: "Робот-пылесос xiaomi", price: 20000, img: 'https://via.placeholder.com/150' },
-//     { title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/150' },
-//     { title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/150' },
-//     { title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/150' },
-// ];
-
-// const renderGoodsItem = (title, price, img = '') => {
-//     return `<div class="goods-item">
-//         <div class="pImage">
-//             <img src="${img}" alt="alt">
-//         </div>
-//         <a class="product_item_link" href="#"><h3>${title}</h3></a>
-//         <p class="price">${price}</p>
-//         <div class="button-block"><button class="btn-buy" name="btn-buy" data-product="0">Купить</button></div>
-//     </div>`
-// };
-
-// const renderGoodsList = (list, container) => {
-//     const goodsList = list.map(good => renderGoodsItem(good.title, good.price, good.img));
-//     document.querySelector(container).innerHTML = goodsList;
-// };
-
-// renderGoodsList(goods, '.goods-list');
